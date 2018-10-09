0c528f0a083d8d21afb8594a5d7c7c0bbed00b77
MasterOC <benediktweyer@gmx.de>
2018-10-09 11:25:31 +0200

    Update onOffIntent.py

--- onOffIntent.py
+++ onOffIntent.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
-from socketIO-client-2 import SocketIO
+from socketIO_client import SocketIO
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
